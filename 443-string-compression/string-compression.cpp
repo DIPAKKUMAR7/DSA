979a54eb370986e26440487ab5bc93d71416f742
DIPAK KUMAR <jaiswaldipak312@gmail.com>
2026-09-06 13:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (16.39%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0;
+        int n = chars.size();
+        int i = 0;
+
+        while( i<n ){
+            int j=i;
+            while(j<n && chars[j] == chars[i]) j++;
+
+            chars[write++] = chars[i];
+
+            int count = j-i;
+
+            if(count >1){
+                string s = to_string(count);
+                for (char c : s) {
+                    chars[write++] = c;
+                }
+            }
+
+            i=j;
+        }
+
+        return write;
+    }
+};
